c17222a3be0cff8cd4f27c93ba731b52e5866219
Rodrigo Nascimento <112453174+rodrigonasciimento@users.noreply.github.com>
2023-10-06 17:54:04 -0300

    adicionando service workerrerer

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  // Adicione mais recursos que deseja cache aqui
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+  );
+});
+
